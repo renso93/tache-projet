6bf7400ac4c6035d54a0bf772e203dd9cb6f27fe
renso93 <renso93.info@gmail.com>
2026-04-07 15:51:39 +0000

    Ajout workflow CI GitHub Actions (après une erreur)

--- auth.py
+++ auth.py
@@ -1,5 +1,5 @@
 from datetime import datetime, timedelta
-from jose  import JWTError, jwt
+from jose import JWTError, jwt
 from passlib.context import CryptContext
 from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
